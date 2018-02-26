bfc5a9684ec1c49c21197fa912c11b8e58cda2d5
Châu Minh Thiện <minhthien1305@gmail.com>
2018-02-26 23:08:31 +0700

    cat layout

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,30 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/dashboard');
+});
+
+Route::get('/dang-nhap.html', 'UserController@getLogin');
+
+Route::group(['prefix' => '/'],function(){
+
+
+	Route::group(['prefix' => 'dashboard'],function(){
+
+        Route::get('/', 'DashboardController@index');
+
+		// Route::get('danh-sach-quyen', 'PrivilegeController@getDSQuyen');
+
+		// Route::get('them-quyen-moi', 'PrivilegeController@getThemQuyen');
+		// Route::post('them-quyen-moi', 'PrivilegeController@postThemQuyen');
+
+		// Route::get('sua-quyen/{id}', 'PrivilegeController@getSuaQuyen');
+		// Route::post('sua-quyen/{id}', 'PrivilegeController@postSuaQuyen');
+
+		// Route::get('xoa-quyen/{id}', 'PrivilegeController@getXoaQuyen');
+
+
+	});
+
+
 });
